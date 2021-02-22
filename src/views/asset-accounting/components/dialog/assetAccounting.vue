d96f3d41d450f951b2c44959250268de6f890ed7
tianguyue <1215232072@qq.com>
2021-02-22 23:19:27 +0800

    资产建帐弹窗

--- src/views/asset-accounting/components/dialog/assetAccounting.vue
+++ src/views/asset-accounting/components/dialog/assetAccounting.vue
@@ -4,47 +4,66 @@
   title="提示"
   :visible.sync="dialogVisible"
      width="60%"
-  :before-close="handleClose">
-  <span>这是一段信息</span>
-  <span slot="footer" class="dialog-footer">
-    <el-button @click="dialogVisible = false">取 消</el-button>
-    <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
-  </span>
-
+ >
+    <basicPage/>
      <template slot="title">
 
       <div class="header_title">
-        <div class="title_font"><span class="title1">资产建账</span><span class=" el-icon-s-platform"></span><span class="title3">收摊成本分摊</span></div>
-        <div class="rightBtn">
+        <div class="title_font"><span class="title1" >资产建账</span><span class=" el-icon-s-platform" style="
+color: #2B57FF;margin-left:18px;"></span><span class="title3" style="
+font-size: 12px;
+font-family: PingFangSC-Regular, PingFang SC;
+font-weight: 400;
+color: #2B57FF;margin-left:3px;cursor: pointer;" @click="toAllocation" >收摊成本分摊</span></div>
+        <!-- <div class="rightBtn">
             <i class=""></i>
-        </div>
+        </div> -->
       </div>
 
     </template>
+
+      <span slot="footer" class="dialog-footer">
+    <el-button size="small" @click="dialogVisible = false">取消返回</el-button>
+    <el-button size="small" type="primary" @click="dialogVisible = false">确认建账</el-button>
+  </span>
+
+  <allocation ref="allocation"/>
 </el-dialog>
 </template>
 
 <script>
+// 基本信息组件
+import basicPage from "./basicPage";
+// 收摊成本分摊
+import allocation from './allocationDialog'
 export default {
     data(){
         return{
             dialogVisible: false
         }
     },
+    components:{
+      basicPage,
+      allocation
+    },
     methods:{
-         handleClose(done) {
-        this.$confirm('确认关闭？')
-          .then(_ => {
-            done();
-          })
-          .catch(_ => {});
+      toAllocation(){
+        this.$refs.allocation.dialogVisible = true
       }
+      //    handleClose(done) {
+      //   this.$confirm('确认关闭？')
+      //     .then(_ => {
+      //       done();
+      //     })
+      //     .catch(_ => {});
+      // }
 
     }
 }
 </script>
 
 <style scoped lang="scss">
+
 //  .specailDialog /deep/ .el-dialog__header {
 // .header_title{   
 //     font-size: 16px;
